acf1ab314d9715f6b7cceb35f5a7f78aec2966ea
Aleks <o.matyka@andersenlab.com>
2024-11-11 19:23:31 +0200

    1

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,10 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en" data-theme="lofi">
-            <head>
-                <link rel="icon" href="/favicon.svg" sizes="any" />
-            </head>
-            <body className="antialiased text-white bg-blue-900">
+        <head>
+            <link rel="icon" href="/favicon.svg" sizes="any" />
+            <meta property="og:alex" content="bro-no-bro" />
+        </head>
+        <body className="antialiased text-white bg-blue-900">
                 <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
                     <div className="flex flex-col w-full max-w-5xl mx-auto grow">
                         <Header />
