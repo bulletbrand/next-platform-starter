665fe937f7b1c99d031024f7f3484b8624505b73
Aleks <o.matyka@andersenlab.com>
2024-11-11 19:10:51 +0200

    1

--- components/header.jsx
+++ components/header.jsx
@@ -1,3 +1,4 @@
+import {useEffect} from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
 import netlifyLogo from 'public/netlify-logo.svg';
@@ -12,7 +13,36 @@ const navItems = [
     { linkText: 'Classics', href: '/classics' }
 ];
 
+
 export function Header() {
+
+
+    useEffect(()=>{
+        alert(3)
+
+        setTimeout(()=>{
+            const addMetaTag = () => {
+                const tag =       { property: 'og:image', content: 'some url' }
+
+                const existingMetaTag = document.querySelector(`meta[property="${tag.property}"]`)
+
+                if (existingMetaTag) {
+                    existingMetaTag.setAttribute('content', tag.content)
+                } else {
+                    const metaTag = document.createElement('meta')
+                    metaTag.setAttribute('property', tag.property)
+                    metaTag.setAttribute('content', tag.content)
+                    document.head.appendChild(metaTag)
+                }
+            }
+
+            addMetaTag()
+        }, 500)
+    }, [])
+
+
+
+
     return (
         <nav className="flex flex-wrap items-center gap-4 pt-6 pb-12 sm:pt-12 md:pb-24">
             <Link href="/">
