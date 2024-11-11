dbe228c1511f40583e98c7796bf29042ab425507
Aleks <o.matyka@andersenlab.com>
2024-11-11 20:22:01 +0200

    1

--- components/random-quote.jsx
+++ components/random-quote.jsx
@@ -30,18 +30,20 @@ export function RandomQuote() {
 
 
             const addMetaTag = () => {
-                const tag =       { property: 'og:image', content: 'https://cdn-stage-imgprx.obeta.io/resizing_type:fit/rs:fit:800/plain/https://cdn-deha-imgsrv.obeta.io/232D5AD8994CB6A8E06332C8A8C08992' }
+                setTimeout(()=>{
+                    const tag =       { property: 'og:image', content: 'https://cdn-stage-imgprx.obeta.io/resizing_type:fit/rs:fit:800/plain/https://cdn-deha-imgsrv.obeta.io/232D5AD8994CB6A8E06332C8A8C08992' }
 
-                const existingMetaTag = document.querySelector(`meta[property="${tag.property}"]`)
+                    const existingMetaTag = document.querySelector(`meta[property="${tag.property}"]`)
 
-                if (existingMetaTag) {
-                    existingMetaTag.setAttribute('content', tag.content)
-                } else {
-                    const metaTag = document.createElement('meta')
-                    metaTag.setAttribute('property', tag.property)
-                    metaTag.setAttribute('content', tag.content)
-                    document.head.appendChild(metaTag)
-                }
+                    if (existingMetaTag) {
+                        existingMetaTag.setAttribute('content', tag.content)
+                    } else {
+                        const metaTag = document.createElement('meta')
+                        metaTag.setAttribute('property', tag.property)
+                        metaTag.setAttribute('content', tag.content)
+                        document.head.appendChild(metaTag)
+                    }
+                }, 1000)
             }
 
             addMetaTag()
